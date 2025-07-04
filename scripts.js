606586610ccd4beed30e59dc8b24ee0a865e47a8
Diego Marcelo <diegomarcelodesouza@gmail.com>
2025-07-04 20:41:03 -0300

    Removendo um item da lista

--- scripts.js
+++ scripts.js
@@ -162,7 +162,13 @@ function updateTotals() {
 expenseList.addEventListener("click", function (event) {
   // Verifica se o elemento clicado é o ícone de remover.
   if(event.target.classList.contains("remove-icon")){
+    // Obtém a li pai do elemento clicado.
+    const item = event.target.closest(".expense")
 
-    console.log(event)
+    // Remove o item da lista.
+    item.remove()
   }
+
+  // Atualiza os totais.
+  updateTotals()
 })
